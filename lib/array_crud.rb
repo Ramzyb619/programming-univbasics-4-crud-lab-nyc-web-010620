4697ad61bb0753277d3d4419311537252baf5693
Ramzy Badawy <ramzy.badawy@gmail.com>
2019-12-18 23:18:57 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,39 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+ food = ["hot dog", "burger", "noodles", "fries"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array (array, element)
+array.push (element)
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def add_element_to_start_of_array (array, element)
+array.unshift (element)
 end
 
-def remove_element_from_end_of_array(array)
-  
+def remove_element_from_end_of_array (array)
+array.pop
 end
 
-def remove_element_from_start_of_array(array)
-  
+def remove_element_from_start_of_array (array)
+array.shift
 end
 
-def retrieve_element_from_index(array, index_number)
-  
+def retrieve_element_from_index (array, index_number)
+array[index_number]
 end
 
-def retrieve_first_element_from_array(array)
-  
+def retrieve_first_element_from_array (array)
+array[0]
 end
 
-def retrieve_last_element_from_array(array)
-  
+def retrieve_last_element_from_array (array)
+array[-1]
 end
 
-def update_element_from_index(array, index_number, element)
-
+def update_element_from_index (array, index_number, new_element_value)
+array[index_number] = new_element_value
 end
